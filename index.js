3770f976c07be21a24808b4f7b877887a125eeb2
Mozibix <opuzmoses@gmail.com>
2023-10-04 21:28:31 +0100

    chores: updated codes

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-hamburger.addEventListener('click', () => {
-    document.getElementByclassName('linksWrapper').style.display = 'none'
-    document.getElementByclassName('navMenu').style.display = 'block'
-   
+hamburger.addEventListener("click", () => {
+  document.getElementByclassName("linksWrapper").style.display = "none";
+  document.getElementByclassName("navMenu").style.display = "block";
 });
